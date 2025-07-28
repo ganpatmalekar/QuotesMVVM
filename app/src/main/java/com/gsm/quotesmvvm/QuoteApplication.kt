6efd44439b5ca29c2d3bf67791dc834cb6ab2049
ganpat.m <ganpat.malekar@paymate.co.in>
2025-07-28 18:11:04 +0530

    1. Changed periodic request time from 15 minutes to 24 hours
    Took 29 minutes

--- app/src/main/java/com/gsm/quotesmvvm/QuoteApplication.kt
+++ app/src/main/java/com/gsm/quotesmvvm/QuoteApplication.kt
@@ -25,7 +25,7 @@ class QuoteApplication : Application() {
     private fun setupWorker() {
         val constraint = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
         val workerRequest = PeriodicWorkRequest
-            .Builder(QuoteWorker::class.java, 15, TimeUnit.MINUTES)
+            .Builder(QuoteWorker::class.java, 24, TimeUnit.HOURS)
             .setConstraints(constraint)
             .build()
         WorkManager.getInstance(this).enqueue(workerRequest)
